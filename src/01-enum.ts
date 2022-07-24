7484049568cbca999eaca7f7d044f1bd7562db74
Jorge Vilca <jlvilcav@gmail.com>
2022-07-23 22:19:01 -0500

    Clase de rest params

--- src/01-enum.ts
+++ src/01-enum.ts
@@ -1,15 +1,15 @@
-enum ROLES {
+export enum ROLES {
   ADMIN = 'admin',
   SELLER = 'seller',
   CUSTOMER = 'customer',
 }
 
-type User = {
+export type User = {
   username: string;
   role: ROLES;
 }
 
-const nicoUser: User = {
+export const nicoUser: User = {
   username: 'nicolas',
   role: ROLES.ADMIN
 }

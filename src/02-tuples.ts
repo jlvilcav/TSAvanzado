92c36837a607e92801548d1ba92a1aa20c2506ab
Jorge Vilca <jlvilcav@gmail.com>
2022-07-23 20:52:50 -0500

    Clase de tuplas

--- src/02-tuples.ts
+++ src/02-tuples.ts
@@ -1,1 +1,17 @@
 const prices: (number|string)[] = [1,2,2,3,3,4,5,'asd'];
+
+prices.push(1);
+prices.push('asd2');
+
+//tupla -> establece la cantidad de elementos como sus tipos de un array
+
+let user:[string, number];
+user = ['jorgebytes',15];
+// user = [12,'jorgebytes'];
+
+const [username, age] = user;
+
+console.log(username);
+console.log(age);
+
+
